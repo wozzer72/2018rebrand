4f62d4303a44a49a24ee56598725f498ca932c88
aylingw <warren.ayling@wozitech-ltd.co.uk>
2018-01-15 09:54:53 +0000

    Added the checkout template

--- forty/assets/js/bjss.js
+++ forty/assets/js/bjss.js
@@ -17,14 +17,14 @@ import {Order, OrderItem} from './bjss/order';
 // initialise the Product Catalogue
 var productCatalogue = new Products();
 productCatalogue.initialise();
-console.log(productCatalogue);
+//console.log(productCatalogue);
 
 // initialise a new Order
 var myOrder = new Order();
 myOrder.add(productCatalogue.products[3], 2);
 myOrder.add(productCatalogue.products[2], 1);
 
-console.log("Order items: " + myOrder.lineItems);
+//console.log("Order items: " + myOrder.lineItems);
 
 var orderTmpl = `{{for orderItems}}
 <tr>
@@ -34,52 +34,51 @@ var orderTmpl = `{{for orderItems}}
 <tr>
 {{/for}}`;
 var tmpl = $.templates(orderTmpl);
-
-var orderHtml = tmpl.render({
-    orderItems : [
-        {
-            product: {
-                name: "Doughnuts"
-            },
-            quantity: 2
-        },
-        {
-            product: {
-                name: "Potatoes"
-            },
-            quantity: 5
-        }
-    ]
-});
-
-var renderOrder = {
-    orderItems : myOrder.lineItems
-};
-
 // jsRender is not able to follow class objects, so need
 //  to export to a simpler object format it can
 var mySimpleOrder = myOrder.export();
+var orderHtml = tmpl.render(mySimpleOrder);
+$("#orderItems").html(orderHtml);
 
-console.log("Render Order: " + mySimpleOrder);
-var orderTwoHtml = tmpl.render(mySimpleOrder);
-console.log("Order generated html: " + orderHtml);
-console.log("Second Order generated html: " + orderTwoHtml);
-//$("#orderItems").html(orderHtml);
-$("#orderItems").html(orderTwoHtml);
+// introducing a format helper for the prices
+// TODO: update the template to use this helper
+var formatCurrency = function(currency, value) {
+    var formattedCurrency = '';
+    switch (currency) {
+        //TODO - add other currencies
+        default:
+            formattedCurrency = "£" + value.toFixed(2);
+            break;
+    }
+    return formattedCurrency;
+}
 
 // display checkout
-var checkoutTemplate = `{{for orderItems}}
+var checkoutTemplate = `<thead>
 <tr>
-    <td>{{>product.name}}</td>
-    <td>{{>quantity}}</td>
-    <td>{{>product.unitPrice}}</td>
-    <td>{{>subTotal}}</td>
-<tr>
-{{/for}}`;
+    <th>Product</th>
+    <th>Quantity</th>
+    <th>Unit Price</th>
+    <th>Subtotal (GBP)</th>
+</tr>
+</thead>
+<tfoot>
+    <th colspan="3">Total</th>
+    <th>{{>total}}</th>
+</tfoot>
+<tbody>
+    {{for orderItems}}
+    <tr>
+        <td>{{>product.name}}</td>
+        <td>{{>quantity}}</td>
+        <td>{{>product.unitPrice}}</td>
+        <td>{{>subTotal}}</td>
+    <tr>
+    {{/for}}
+</tbody>`;
 var checkoutTmpl = $.templates(checkoutTemplate);
-
+// add the order total for checkout
 mySimpleOrder.total = myOrder.total;
-console.log("The order total: " + mySimpleOrder.total);
-
 var checkoutHtml = checkoutTmpl.render(mySimpleOrder);
-$("#checkoutItems").html(checkoutHtml);
+$("#checkoutTbl").html(checkoutHtml);
+
